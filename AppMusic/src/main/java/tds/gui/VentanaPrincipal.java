84b3ea4348ae92b748b4bafed2fe4c52b1f5dc83
Joaquín <Joaqun@DESKTOP-EVJSU3C>
2020-11-15 18:10:20 +0100

    Parte izquierda ventana principal 1

--- AppMusic/src/main/java/tds/gui/VentanaPrincipal.java
+++ AppMusic/src/main/java/tds/gui/VentanaPrincipal.java
@@ -10,6 +10,11 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 
 public class VentanaPrincipal {
 
@@ -40,10 +45,37 @@ public class VentanaPrincipal {
 		frmVentanaPrincipal.getContentPane().add(panel, BorderLayout.NORTH);
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("Hola jesus");
 		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Mejora tu cuenta");
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Logout");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnNewButton_1);
+		
+		JPanel panel_1 = new JPanel();
+		frmVentanaPrincipal.getContentPane().add(panel_1, BorderLayout.WEST);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+		
+		JLabel lblNewLabel_1 = new JLabel("Explorar");
+		/*lblNewLabel_1.setIcon(new ImageIcon(getClass().getResource("/iconoboton.png")));
+		lblNewLabel_1.setMaximumSize(new Dimension(80,45));
+		lblNewLabel_1.setMinimumSize(new Dimension(80,45));*/
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nueva lista");
+		panel_1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		panel_1.add(lblNewLabel_3);
 		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
+
 		
 		
 		/*JLabel labelCompartirCoche = new JLabel("Bienvenidos a AppMusic");
