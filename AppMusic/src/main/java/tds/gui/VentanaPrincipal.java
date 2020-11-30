9c5d2b6a4de9d78bcd758971fcfed50ec77fbb12
lauriverdu <averdu8@gmail.com>
2020-11-30 18:19:47 +0100

    Botones play

--- AppMusic/src/main/java/tds/gui/VentanaPrincipal.java
+++ AppMusic/src/main/java/tds/gui/VentanaPrincipal.java
@@ -2,17 +2,17 @@ package tds.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
 
+import beans.Entidad;
 import tds.controlador.Controlador;
 import tds.dominio.CatalogoUsuarios;
 import tds.dominio.Usuario;
+import tds.driver.ServicioPersistencia;
 
 import java.awt.FlowLayout;
 import javax.swing.JButton;
@@ -28,12 +28,14 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 
+
 public class VentanaPrincipal {
 
 	private JFrame frmVentanaPrincipal;
 	private JTextField txtTtulo;
 	private JTextField txtIntrprete;
 	private Usuario usuarioActual = Controlador.getUnicaInstancia().getUsuarioActual();
+
 	public VentanaPrincipal() {
 		initialize();
 	}
@@ -44,89 +46,164 @@ public class VentanaPrincipal {
 		frmVentanaPrincipal.setVisible(true);
 	}
 	
-	public void initialize() {
-		frmVentanaPrincipal = new JFrame();
-		frmVentanaPrincipal.getContentPane().setBackground(Color.WHITE);
-		frmVentanaPrincipal.setTitle("AppMusic - Ventana Principal");
-		frmVentanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private JPanel crearPanel1() {
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(Color.WHITE);
+		frmVentanaPrincipal.getContentPane().add(panel1, BorderLayout.NORTH);
+		panel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		frmVentanaPrincipal.setSize(400, 400);
-		frmVentanaPrincipal.setMinimumSize(new Dimension(400, 400));
 
-		JPanel contentPane = (JPanel) frmVentanaPrincipal.getContentPane();
-		frmVentanaPrincipal.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		frmVentanaPrincipal.getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JPanel panelMedio = new JPanel();
-		frmVentanaPrincipal.getContentPane().add(panelMedio, BorderLayout.CENTER);
-		
-		JPanel panelExplorar = new JPanel();
-		frmVentanaPrincipal.getContentPane().add(panelExplorar);
-		panelExplorar.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_2 = new JPanel();
-		panelExplorar.add(panel_2, BorderLayout.NORTH);
-		
-		txtIntrprete = new JTextField();
-		txtIntrprete.setText("Intérprete");
-		panel_2.add(txtIntrprete);
-		txtIntrprete.setColumns(10);
-		
-		txtTtulo = new JTextField();
-		txtTtulo.setText("Título");
-		panel_2.add(txtTtulo);
-		txtTtulo.setColumns(10);
-		
-		JComboBox comboBox = new JComboBox();
-		panel_2.add(comboBox);
-		
-	
-		
-	
-		
-		
-		
-		//JLabel lblNewLabel = new JLabel("Hola ");
 		JLabel lblNewLabel = new JLabel("Hola " + usuarioActual.getNombre());
-		panel.add(lblNewLabel);
+		panel1.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Mejora tu cuenta");	
-		panel.add(btnNewButton);
+
+		JButton btnNewButton = new JButton("Mejora tu cuenta");
+		panel1.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Logout");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton_1);
+		panel1.add(btnNewButton_1);
 		
+		return panel1;
+	}
+	
+
+	/*private JPanel crearPanelCancion(JPanel panelMedio, JPanel panelBotones) {
+		JPanel panelCancion = new JPanel();
+		panelCancion.setLayout(new BorderLayout(0, 0));
+		ImageIcon icnPlay = new ImageIcon(getClass().getResource("/pause.png"));
+
+		JButton btnPlay = new JButton("play", icnPlay);
+		btnPlay.setMinimumSize(new Dimension(100,20));
+
+		panelCancion.add(btnPlay, BorderLayout.NORTH);
+		
+		
+	
+		ImageIcon icnPause = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnPause = new JButton("pause", icnPause);
+		btnPause.setMinimumSize(new Dimension(100,20));
+		btnPause.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+		btnPause.setBackground(Color.WHITE);
+		panelCancion.add(btnPause);
+		
+	
+		
+		ImageIcon icnAnterior= new ImageIcon(getClass().getResource("/pause.png"));
+
+		JButton btnAnterior = new JButton("Anterior", icnAnterior);
+		btnAnterior.setMinimumSize(new Dimension(100,20));
+
+		panelCancion.add(btnAnterior, BorderLayout.WEST);
+		
+	
+		
+		ImageIcon icnSiguiente= new ImageIcon(getClass().getResource("/pause.png"));
+
+		JButton btnSiguiente = new JButton("Siguiente", icnSiguiente);
+		btnSiguiente.setMinimumSize(new Dimension(100,20));
+
+		panelCancion.add(btnSiguiente, BorderLayout.EAST);
+		return panelCancion;
+	}
+	*/
+	
+	private JPanel crearPanelMedio() {
+		JPanel panelMedio = new JPanel();
+		frmVentanaPrincipal.getContentPane().add(panelMedio, BorderLayout.CENTER);
+		panelMedio.setLayout(new BorderLayout(0, 0));
+	
+		JPanel panel_sur_play = new JPanel();
+		panelMedio.add(panel_sur_play, BorderLayout.SOUTH);
+		panel_sur_play.setLayout(new BorderLayout(0, 0));
+		
+		ImageIcon icnPlay = new ImageIcon(getClass().getResource("/play.png"));
+
+		JButton btnPlay = new JButton(icnPlay);
+		btnPlay.setMinimumSize(new Dimension(100,20));
+		btnPlay.setMaximumSize(new Dimension(100,20));
+
+		panel_sur_play.add(btnPlay, BorderLayout.NORTH);
+		
+		
+	
+		ImageIcon icnPause = new ImageIcon(getClass().getResource("/pausa.png"));
+		JButton btnPause = new JButton( icnPause);
+		btnPause.setMinimumSize(new Dimension(100,20));
+		btnPause.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+		btnPause.setBackground(Color.WHITE);
+		panel_sur_play.add(btnPause);
+		
+	
+		
+		ImageIcon icnAnterior= new ImageIcon(getClass().getResource("/anterior.png"));
+
+		JButton btnAnterior = new JButton(icnAnterior);
+		btnAnterior.setMinimumSize(new Dimension(100,20));
+
+		panel_sur_play.add(btnAnterior, BorderLayout.WEST);
+		
+	
+		
+		ImageIcon icnSiguiente= new ImageIcon(getClass().getResource("/siguiente.png"));
+
+		JButton btnSiguiente = new JButton( icnSiguiente);
+		btnSiguiente.setMinimumSize(new Dimension(100,20));
+
+		panel_sur_play.add(btnSiguiente, BorderLayout.EAST);
+		
+	/*	ImageIcon icnPlay = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnPlay = new JButton("pause", icnPlay);
+		btnPlay.setMinimumSize(new Dimension(100,20));
+	//	btnPlay.setAlignmentX(Component.TOP_ALIGNMENT);
+		btnPlay.setBackground(Color.WHITE);
+		panel_sur_play.add(btnPlay);
+		
+		
+		
+		ImageIcon icnPause = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnPause = new JButton("pause", icnPause);
+		btnPause.setMinimumSize(new Dimension(100,20));
+		btnPause.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+		btnPause.setBackground(Color.WHITE);
+		panel_sur_play.add(btnPause);
+		
+		
+
+		
+		ImageIcon icnAnterior = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnAnterior = new JButton("anterior", icnAnterior);
+		btnAnterior.setMinimumSize(new Dimension(100,20));
+		//btnAnterior.setAlignmentX(Component.LEFT_ALIGNMENT);
+		btnAnterior.setBackground(Color.WHITE);
+
+		panel_sur_play.add(btnAnterior);
+		
+		
+		
+		
+		
+		ImageIcon icnSiguiente = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnSiguiente = new JButton("anterior", icnSiguiente);
+		btnSiguiente.setMinimumSize(new Dimension(100,20));
+	//	btnSiguiente.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		btnSiguiente.setBackground(Color.WHITE);
+
+		panel_sur_play.add(btnSiguiente);
+	*/
+		return panelMedio;
+	}
+	private JPanel crearPanelBotones(JPanel panelMedio, JPanel panelExplorar) {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.WHITE);
 		frmVentanaPrincipal.getContentPane().add(panel_1, BorderLayout.WEST);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
-		
 		
 		ImageIcon icnExplorar = new ImageIcon(getClass().getResource("/lupa.png"));
 		JButton btnExplorar = new JButton("Explorar", icnExplorar);
 		btnExplorar.setMinimumSize(new Dimension(100, 20));
 		btnExplorar.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnExplorar.setBackground(Color.WHITE);
-		btnExplorar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				panelMedio.removeAll();
-				panelMedio.add(panelExplorar);
-				panelMedio.revalidate();
-				panelMedio.repaint();
-				}
-		});
-	
 		panel_1.add(btnExplorar);
-		
 		
 		
 		JSeparator separator = new JSeparator();
@@ -159,7 +236,7 @@ public class VentanaPrincipal {
 		btnRecientes.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnRecientes.setMinimumSize(new Dimension(100, 20));
 		panel_1.add(btnRecientes);
-		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+		//contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
 		JSeparator separator3 = new JSeparator();
 		separator3.setForeground(Color.WHITE);
@@ -177,13 +254,101 @@ public class VentanaPrincipal {
 		btnListas.setAlignmentX(Component.CENTER_ALIGNMENT);
 		panel_1.add(btnListas);
 		
+		addManejadorBotonExplorar(btnExplorar, panelMedio, panelExplorar);
+				
+		return panel_1;
+		
+		
+	}
+	
+	public JPanel crearPanelExplorarNorte() {
+		JPanel panelExplorarNorte = new JPanel();
+		txtIntrprete = new JTextField();
+		txtIntrprete.setText("Intérprete");
+		panelExplorarNorte.add(txtIntrprete);
+		txtIntrprete.setColumns(10);
+		
+		txtTtulo = new JTextField();
+		txtTtulo.setText("Título");
+		panelExplorarNorte.add(txtTtulo);
+		txtTtulo.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		panelExplorarNorte.add(comboBox);
+		
+		return panelExplorarNorte;
+	}
+	
+/*	public JPanel crearPanelExplorarCentro() {
+		JPanel panelExplorarCentro = new JPanel();
+		ImageIcon icnPause = new ImageIcon(getClass().getResource("/pause.png"));
+		JButton btnPause = new JButton("Pause", icnPause);
+		btnPause.setBackground(Color.WHITE);
+		btnPause.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnPause.setMinimumSize(new Dimension(100, 20));
+		//panelExplorarCentro.add(btnPause);
+		JComboBox comboBox = new JComboBox();
+		panelExplorarCentro.add(comboBox);
+		
+		
+		return panelExplorarCentro;
 
-		/*JLabel labelCompartirCoche = new JLabel("Bienvenidos a AppMusic");
-		labelCompartirCoche.setFont(new Font("Arial", Font.PLAIN, 30));
-		labelCompartirCoche.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(labelCompartirCoche, BorderLayout.CENTER);*/
+
+	}*/
+	
+	public JPanel crearPanelExplorar(JPanel panelExplorarNorte) {
+		JPanel panelExplorar = new JPanel();
+		panelExplorar.setLayout(new BorderLayout(0, 0));
+		panelExplorar.add(panelExplorarNorte, BorderLayout.NORTH);
+		return panelExplorar;
+	}
+	
+	
+	
+	public void addManejadorBotonExplorar(JButton btnExplorar, JPanel panelMedio, JPanel panelExplorar) {
+		btnExplorar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panelMedio.removeAll();
+				panelMedio.add(panelExplorar);
+				panelMedio.revalidate();
+				panelMedio.repaint();
+				}
+		});
+	}
+	
+	public void addManejadorBotonLogout(JButton btnLogout, JPanel panel1) {
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	}
+	
+
+	
+	public void initialize() {
+		frmVentanaPrincipal = new JFrame();
+		frmVentanaPrincipal.getContentPane().setBackground(Color.WHITE);
+		frmVentanaPrincipal.setTitle("AppMusic - Ventana Principal");
+		frmVentanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		frmVentanaPrincipal.setSize(400, 400);
+		frmVentanaPrincipal.setMinimumSize(new Dimension(400, 400));
+
+		JPanel contentPane = (JPanel) frmVentanaPrincipal.getContentPane();
+		frmVentanaPrincipal.getContentPane().setLayout(new BorderLayout(0, 0));
 		
+		JPanel panel1 = crearPanel1();
+		
+		JPanel panelMedio = crearPanelMedio();
+		
+		JPanel panelExplorarNorte = crearPanelExplorarNorte();
+		//JPanel panelExplorarCentro = crearPanelExplorarCentro();
+		JPanel panelExplorar = crearPanelExplorar(panelExplorarNorte);
+		//JPanel panelExplorar2 = crearPanelExplorar(panelExplorarCentro);
+		JPanel panelBotones = crearPanelBotones(panelMedio, panelExplorar);
+		//JPanel panelBotones2 = crearPanelBotones(panelMedio, panelExplorar2);
+
 		frmVentanaPrincipal.pack();
 	}
 
